117db6a83aa373cfc4a39482dad6a09b67edd9c9
Kenneth Lawrence <icariumdesign@gmail.com>
2014-03-02 14:59:47 -0600

    Tiny fixes

--- src/application/src/js/controllers/_resultsController.js
+++
@@ -1,7 +0,0 @@
-.controller('resultsController', ['$scope', 'matchMakerService',
-  function($scope, matchMakerService) {
-
-    $scope.results = matchMakerService.getLatestResults();
-    console.log($scope.results);
-  }
-])
